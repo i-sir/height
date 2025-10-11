6807274a3349b7d87bbcd39d2eb52e71e29846cc
恏 <3031453390@qq.ccom>
2025-10-11 17:29:13 +0800

    ``` feat(CoursePlanInit): 添加课程计划初始化时的全局解锁状态字段
    在课程计划初始化逻辑中，新增了 is_all_unlock 字段来表示当前阶段是否
    处于全局解锁状态。该字段来源于传入参数 params 中的 is_all_unlock 值，用于更准确地控制课程阶段的解锁状态显示与逻辑处理。
    ```

--- extend/init/CoursePlanInit.php
+++ extend/init/CoursePlanInit.php
@@ -69,8 +69,8 @@ class CoursePlanInit extends Base
             }
         }
         if ($params['is_all_unlock'] == true) $item['is_unlock'] = 2; // 上个阶段未学习完成,这个阶段全部锁定
-
         $item['is_unlock_name'] = $this->is_unlock[$item['is_unlock']];
+        $item['is_all_unlock']  = $params['is_all_unlock'];
 
 
         /** 处理文字描述 **/
